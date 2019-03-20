f6a6ab16e396b7b6948df16972b1a939bf9d2b55
fredcorpuz06 <fcorpuz@wesleyan.edu>
2019-03-20 04:44:00 -0400

    Upload data to PostGreSQL, attempt to link to app

--- scratch.py
+++ scratch.py
@@ -1,22 +1,131 @@
 import pandas as pd
 import numpy as np
+import sqlite3
+from sqlalchemy import create_engine
+import os 
+import psycopg2
+
+# DATABASE_URL = os.environ['DATABASE_URL']
+# DATABASE_URL = "postgresql-dimensional-36261"
+
+
+# cmd = "heroku config:get DATABASE_URL"
+# DATABASE_URL = os.popen(cmd).read()
+# print(DATABASE_URL)
+DATABASE_URL = "host=ec2-50-17-231-192.compute-1.amazonaws.com dbname=dbpcnq9cirtphf user=rnkbpafrerjisd  password=57c384efff4206b3556c7b33fd99a3e936a856658b3ebc5870a204e2a8dea9a9 sslmode=require"
+# conn = psycopg2.connect(
+#     host="ec2-50-17-231-192.compute-1.amazonaws.com",
+#     dbname="dbpcnq9cirtphf",
+#     user="rnkbpafrerjisd",
+#     password="57c384efff4206b3556c7b33fd99a3e936a856658b3ebc5870a204e2a8dea9a9",
+#     port=5432,
+#     sslmode="require"
+# )
+conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
+cur.execute('CREATE TABLE test (id PRIMARY KEY, num INTEGER, data VARCHAR);')
+cur.execute('INSERT INTO test (num, data) VALUES (100, "abc");')
+cur.execute('SELECT * FROM test;')
+rez = cur.fetchall()
+print(rez)
+
+conn.commit()
+conn.close()
+
+
+
+# conn = sqlite3.connect("./fred.db")
+# conn = sqlite3.connect(":memory:")
+# engine = create_engine('sqlite:///fred.db', echo=False)
+# c = conn.cursor()
+# c.execute("SELECT * FROM funds")
+# funds1 = c.fetchall()
+# print(funds1[:5])
+# funds1 = pd.read_sql_query("SELECT * FROM funds", conn)
+# print(funds1.head())
+
+# funds = pd.read_csv("./data/funds_clean.csv")
+# funds.to_sql('funds', con=engine)
+
+
+
+# c.execute("""CREATE TABLE employees (
+#             first TEXT,
+#             last TEXT,
+#             pay INTEGER
+#             )""")
+
+# def insert_emp(emp):
+#     with conn:
+#         c.execute(
+#             "INSERT INTO employees VALUES (:first, :last, :pay)",
+#             {'first': emp.first, 'last': emp.last, 'pay': emp.pay}
+#         )
+
+# def get_emps_by_name(lastname):
+#     c.execute(
+#         "SELECT * FROM employees WHERE last=:last",
+#         {'last': lastname}
+#     )
+#     return c.fetchall()
+
+# def update_pay(emp, pay):
+#     with conn:
+#         c.execute(
+#             """UPDATE employees SET pay=:pay
+#               WHERE first = :first AND last = :last""",
+#             {'first': emp.first, 'last': emp.last, 'pay': emp.pay}
+#         )
+
+# def remove_emp(emp):
+#     with conn:
+#         c.execute(
+#             "DELETE FROM employees WHERE first = :first AND last = :last",
+#             {'first': emp.first, 'last': emp.last}
+#         )
+
+
+# class Employee:
+#     def __init__(self, first, last, pay):
+#         self.first = first
+#         self.last = last
+#         self.pay = pay
+    
+#     @property
+#     def email(self):
+#         return '{}.{}@email.com'.format(self.first, self.last)
+
+#     @property
+#     def fullname(self):
+#         return '{} {}'.format(self.first, self.last)
+
+#     def __repr__(self):
+#         return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+# emp_1 = Employee('John', 'Doe', 80000)
+# emp_2 = Employee('Jane', 'Doe', 90000)
+
+# c.execute("SELECT * FROM fredsTable")
+# print(c.fetchall())
+
+# insert_emp(emp_1)
+# insert_emp(emp_2)
+
+# emps = get_emps_by_name('Doe')
+# print(emps)
+
+# update_pay(emp_2, 95000)
+# remove_emp(emp_1)
+
+# emps = get_emps_by_name('Doe')
+# print(emps)
+
+
+# conn.close()
+ 
+
 
 """ 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-
-df[' index'] = range(1, len(df) + 1)
-# print(df)
-
-page_settings={'current_page': 0, 'page_size': 5}
-
-startP = page_settings['current_page'] * page_settings['page_size']
-endP = (page_settings['current_page'] + 1) * page_settings['page_size']
-
-print(startP, endP)
-print(df.iloc[startP:endP])
-print(df.iloc[startP:endP].to_dict('rows'))
-
- """
 grants = pd.read_csv("./data/grants_clean.csv")
 funds = pd.read_csv("./data/funds_clean.csv")
 
@@ -82,55 +191,4 @@ for k, f in flows.items():
     print(k, len(f))
 pd.DataFrame(flows).to_csv('./data/scratch3.csv', index=False)
 
-
-'''
-dff = grants[(grants.year >= yearRange[0]) & (grants.year <= yearRange[1])]
-# dff = dff[[varChoice1, varChoice2, 'grant_damt']]
-dff = dff[['year', varChoice1, varChoice2, 'grant_damt']]
-
-g = dff.groupby([varChoice1, 'year'])
-# g = dff.groupby([varChoice1, varChoice2])
-rez = g.agg([np.sum, lambda x: np.shape(x)[0], np.mean]).rename(columns={
-            'sum': summary_types[0],
-            'mean': summary_types[2],
-            '<lambda>': summary_types[1],
-        })['grant_damt']
-
-# print(rez['grant_damt'].gross_total)
-
-print(rez)
-# bars =[{
-#     'name': varChoice1,
-#     'label': rez.index.tolist(),
-#     'value': rez[summaryType].tolist()
-# }]
-# print(bars)
-# .unstack(fill_na=0).stack()
-
-bars = []
-for name, group in rez.groupby(level=0):
-    bar = {
-        'name': name,
-        'label': [i[1] for i in group.index],
-        'value': group[summaryType].tolist(),
-    }
-    bars.append(bar)
-print(bars)
-
-# bars = [
-#     {
-#         'label': rez.index.tolist(),
-#         'value': rez[summaryType].tolist()
-#     },
-#     {
-#         'label': ['a','b','c'],
-#         'value': [4,5,6]
-#     },
-
-# ]
-# print(bars)
-
-# dff = grants[(grants.year >= yearRange[0]) & (grants.year <= yearRange[1])]
-# dff = dff[[varChoice1, varChoice2, 'grant_damt']]
-'''
-
+"""
