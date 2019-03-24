171aae06b6d0d939ab54b2871abb53cfec55088d
fredcorpuz06 <fcorpuz@wesleyan.edu>
2019-03-24 15:50:24 -0400

    try fruit 4

--- apps/fruits.py
+++ apps/fruits.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import psycopg2
 import os
+import pandas as pd
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -23,6 +24,11 @@ cur.execute('SELECT value FROM forluck;')
 value = cur.fetchall()
 value = [v[0] for v in value]
 
+print('Without pandas:', label, value)
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+forluck = pd.read_sql_query("SELECT * FROM forluck;", conn)
+print('With pandas:', forluck.label, forluck.value)
+
 from app import app
 
 layout = html.Div(children=[
@@ -39,7 +45,7 @@ layout = html.Div(children=[
        figure=go.Figure(
             data=[
                 go.Bar(
-                x=label, y=value, name='SF'),
+                x=forluck.label, y=forluck.value, name='SF'),
                 # x=[1, 2, 3], y=[2, 4, 5], name='SF')
             ],
 
